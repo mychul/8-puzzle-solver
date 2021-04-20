8c3dfcaee0a7018389c4db736eb161b7c379b9e9
Michael Chiang <mychiang13@gmail.com>
2021-04-19 17:35:50 -0700

    node

--- Node.py
+++ Node.py
@@ -1,8 +1,13 @@
-class Node:
-    def __init__(self, obj): 
-        self.obj = obj
+from problems import problems
+
+class node(problems):
+    def __init__(self):
+        self.parent = None
+        self.aboveChild = None
+        self.belowChild = None
+        self.rightChild = None
+        self.leftChild = None
+        self.state = state
     def get(self):  return self.obj
     def set(self, obj):     self.obj = obj
     #Node parent = self.obj
-    
-    
