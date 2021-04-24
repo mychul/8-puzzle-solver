10f01692a209b183d1651d290aac09e39730ea5a
TinhLa72 <tla005@ucr.edu>
2021-04-23 18:30:21 -0700

    Update

--- 8PuzzleSolver.py
+++ 8PuzzleSolver.py
@@ -2,6 +2,7 @@ from array import *
 from Problems import *
 from Node import *
 from Misplaced import *
+from Eucledian import *
 pad = [0,0,0]
 
 print("Welcome to 861126014 & 861199635 8 puzzle solver.")    
@@ -49,10 +50,13 @@ elif choice == 2:
         pad[i][1] = y
         pad[i][2] = z
 choice = input("/nEnter your choice of algorithm\n1 - Uniform Cost Search\n2- A* with the Misplaced Tile heuristic.\n3 - A* with the Eucledian distance heuristic.\n\nChoice: ")
-print("")
-print("Expanding state: ")
-initial_problem = problems(pad) 
-initial_problem.printState()
+choice = int(choice)
+inital_node = node(pad)
+if(choice == 3):
+    print(eucledian_distance(inital_node))
+
+#initial_problem = problems(pad) 
+#initial_problem.printState()
 #testNode= node(pad)
 #testNode.spawnChild()
 #testNode.printChildren()
