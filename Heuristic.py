180e0be52462314cc78b01bf63da0dd12ace2f75
Michael Chiang <mychiang13@gmail.com>
2021-04-23 17:57:09 -0700

    Update Heuristic.py

--- Heuristic.py
+++ Heuristic.py
@@ -5,6 +5,8 @@ class Heurisitic:
     def __init__(self,problem):
         start = problem
         explored={start.initial_state}
+        expanded=0
+        max_size=0
         
 
     #helper function to check if node up for consideration has already been previously explored or not
@@ -13,6 +15,10 @@ class Heurisitic:
             return True
         else:
             return False
+    def checkMax(self,curr_size):
+        if len(curr_size) > self.max_size:
+            self.maxsize = len(curr_size)
+        return
 
     #helper function that traces route to solution node and saves into a list        
     def traceSolution(self,node):
@@ -28,5 +34,8 @@ class Heurisitic:
     def goalReached(self):
         path = self.traceSolution()
         path.reverse()
+        self.expanded = len(path)
         for x in path:
             print (path[x])
+        print ("To solve this problem the serach algorithm expanded " + self.expanded + " nodes.")
+        print ("The maximum number of nodes at any one time was: " + self.max_size)
