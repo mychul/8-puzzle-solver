360448291e47896e5a1f3596ca1eca3aa11cd06e
TinhLa72 <tla005@ucr.edu>
2021-04-24 19:00:56 -0700

    Update Eucledian.py

--- Eucledian.py
+++ Eucledian.py
@@ -49,3 +49,48 @@ class Eucledian(Heuristic):
                 done.append(value1)                
                 count = count + 1
         return sum
+    def AStarSearch(self):
+        f = 0
+        self.frontier.put((f, self.nodeCounter, self.cur))
+        while not self.frontier.empty():
+            self.cur = self.frontier.get()[2]
+            self.cur.spawnChild()
+            if self.cur.leftChild is not None:
+                if(self.cur.leftChild.checkGoal()):
+                    self.cur = self.cur.leftChild
+                    self.goalFlag = True
+                    break
+                self.nodeCounter = self.nodeCounter + 1
+                h = self.eucledian_distance(self.cur.leftChild)
+                f = h + self.cur.leftChild.g
+                self.frontier.put((f, self.nodeCounter, self.cur.leftChild)) 
+            if self.cur.rightChild is not None:
+                if(self.cur.rightChild.checkGoal()):
+                    self.cur = self.cur.rightChild
+                    self.goalFlag = True
+                    break
+                self.nodeCounter = self.nodeCounter + 1
+                h = self.eucledian_distance(self.cur.rightChild)
+                f = h + self.cur.rightChild.g
+                self.frontier.put((f, self.nodeCounter, self.cur.rightChild)) 
+            if self.cur.aboveChild is not None:
+                if(self.cur.aboveChild.checkGoal()):
+                    self.cur = self.cur.aboveChild
+                    self.goalFlag = True
+                    break
+                self.nodeCounter = self.nodeCounter + 1
+                h = self.eucledian_distance(self.cur.aboveChild)
+                f = h + self.cur.above.g
+                self.frontier.put((f, self.nodeCounter, self.cur)) 
+            if self.cur.belowChild is not None:
+                if(self.cur.belowChild.checkGoal()):
+                    self.cur = self.cur.belowChild
+                    self.goalFlag = True
+                    break
+                self.nodeCounter = self.nodeCounter + 1
+                h = self.eucledian_distance(self.cur.belowChild)
+                f = h + self.cur.belowChild.g
+                self.frontier.put((f, self.nodeCounter, self.cur.belowChild)) 
+        if(self.goalflag):
+            return self.goalFlag
+        return self.goalFlag
